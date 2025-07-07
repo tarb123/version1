10b03cc06915be3daf4e5cdea9e743a5f26d9d02
Tarb Mohsin <tarbmohsin18@gmail.com>
2025-07-07 16:22:43 +0500

    🔧 Update Tailwind classes in multiple files

--- src/app/components/nav/ResponsiveNav.tsx
+++ src/app/components/nav/ResponsiveNav.tsx
@@ -4,8 +4,16 @@ import Header from './Header'
 const ResponsiveNav = () => {
   return (
     <div>
-        <Header/>  
+      <Header />
+
+      {/* ⬇️ This contains the actual content below the fixed mobile header */}
+      <div className="mainContent">
+        {/* <h1>Welcome to Sanjeeda!</h1> */}
+        {/* <p>This is your home page content. Replace this with your components or sections.</p> */}
+        {/* More components or sections here */}
+      </div>
     </div>
   )
 }
+
 export default ResponsiveNav
